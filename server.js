22651b779cc793f88918ee5e9811b673e51d937a
aashis-kishore <aashiskishore@gmail.com>
2021-02-09 03:12:55 +0530

    Adapt to pm2 and non-pm2 serving

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = require('./app');
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.info('Listening on port ' + listener.address().port);
   // Inform pm2 that the app is ready to be served
-  process.send('ready');
+  if (process.send) {
+    process.send('ready');
+  }
 });
 
 // Graceful shutdown
 process.on('SIGINT', () => {
   // Clear everything needed.
+  console.info('Disposing server, bye');
   listener.close(() => process.exit(0));
 });
