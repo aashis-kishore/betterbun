2680c1392669c5af41d1f740c2e27b0fb6d6e896
aashis-kishore <aashiskishore@gmail.com>
2021-05-13 17:45:54 +0530

    Format linter configuration file

--- test/integration/errors.test.js
+++ test/integration/errors.test.js
@@ -1,42 +1,29 @@
-const mongoose = require('mongoose');
-const request = require('supertest');
+const supertest = require('supertest');
 
 const app = require('../../src/app');
 
-// describe('Edge case routes', () => {
-describe('Requests to non-existent resources', () => {
-  beforeAll(async () => {
-    mongoose.Promise = Promise;
-    const mongoUri = 'mongodb://mongoadmin:imtheadmin@mongodb/betterbun-db-test?authSource=admin';
-    mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+const request = supertest(app);
+
+describe('Edge case routes', () => {
+  describe('Requests to non-existent resources', () => {
+    it('should have response status 404', async (done) => {
+      await request
+        .get('/non/existent/route')
+        .expect(404);
+
+      done();
     });
-    mongoose.connection.on('error', () => {
-      throw new Error(`Unable to connect to database: ${mongoUri}`);
+
+    it('should have expected response message', async (done) => {
+      await request
+        .get('/non/existent/route')
+        .expect(res => {
+          const data = res.body;
+          expect(data.errors[0].message)
+            .toMatch('Resource not found');
+
+          done();
+        });
     });
-  });
-
-  afterAll(async () => {
-    await mongoose.connection.close();
-  });
-
-  it('should have response status 404', async (done) => {
-    await request(app)
-      .get('/non/existent/route')
-      .expect(404);
-
-    done();
-  });
-
-  it('should have expected response message', async (done) => {
-    await request(app).get('/non/existent/route')
-      .expect(res => {
-        const data = res.body;
-        expect(data.errors[0].message).toMatch('Resource not found');
-
-        done();
-      });
   });
 });
-// });
